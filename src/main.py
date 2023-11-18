c93c9567fa8e6dfc797c730f0e1deb6ce2df1217
Lucky8boy <robert.nenciu@yahoo.com>
2023-11-19 01:56:39 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import mainMenu
 import pyglet
-import gameUtils
 import constant
 import map
 
@@ -9,13 +8,6 @@ pyglet.resource.path = ["../assets"] # assets needs to be the default resource d
 pyglet.resource.reindex()
 
 main_menu = mainMenu.MainMenu()
-
-player = pyglet.media.Player()
-player.queue(pyglet.media.load(gameUtils.getFilePath('Main_Menu.wav', constant.TYPE_SONG)))
-
-player.volume = 0
-player.play()
-player.loop = True
 # this
 
 map = map.Map(constant.MAP1_PATH)
