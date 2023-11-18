18f331c905774fe060abdb095c74d68693104a95
Lucky8boy <robert.nenciu@yahoo.com>
2023-11-18 21:57:41 +0200

    Create option.py
    - used for settings tab

---
+++ src/option.py
@@ -0,0 +1,43 @@
+import pyglet
+import gameUtils
+import constant
+
+class Option():
+    x = None        # coords
+    y = None
+
+    label = None    # main text
+
+    image = None    # display the button
+    sprite = None
+
+    batch = None    # graphics
+
+    def __init__(self, name: str, x: int, y: int, batch: pyglet.graphics.Batch):
+
+        # gameUtils.getFilePath('grey_button.png', '\\assets\img\\')
+        self.image = pyglet.image.load(gameUtils.getFilePath(constant.TEXT_BG_PATH, constant.TYPE_IMAGE))
+
+        self.sprite = pyglet.sprite.Sprite(self.image, x = x, y = y, z = 101, batch = batch)
+
+        self.label = pyglet.text.Label(name,
+                                       font_name = 'Times New Roman',
+                                       font_size = 28,
+                                       x = x + 150,
+                                       y = y + 25,
+                                       z = 100,
+                                       anchor_x = 'center', anchor_y = 'center',
+                                       batch = batch)
+
+        self.x = x
+        self.y = y
+
+        self.batch = batch
+
+    def hide(self):
+        self.sprite.batch = None
+        self.label.batch = None
+
+    def show(self):
+        self.sprite.batch = self.batch
+        self.label.batch = self.batch
