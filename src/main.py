aa94d80a4707e6a81e2c4a71f4a7f50c5dd19668
Lucky8boy <robert.nenciu@yahoo.com>
2024-01-06 14:49:38 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import map
 import enemy
 import building
 
-
 # do not change
 pyglet.resource.path = ["../assets"] # assets needs to be the default resource directory
 pyglet.resource.reindex()
@@ -13,29 +12,14 @@ pyglet.resource.reindex()
 main_menu = mainMenu.MainMenu()
 # this
 
-map = map.Map(constant.MAP1_PATH)
+map = map.Map(constant.MAP1_PATH, main_menu.batch)
 
 if not map.isValid:
     exit(1)
 
 # enemy test (can be removed)
-e = enemy.Enemy("name", "color", 0.1, 1, 1, 1, 1, main_menu.batch)
-result = e.newCoordinates(360, 290, map.matrix)
-
-while not type(result) == int:
-    x, y = result
-    result = e.newCoordinates(x, y, map.matrix)
-
-# building test (can be removed)
-b = building.Building("name", constant.SQUARE, 5, 5.0, 50, 50, 100, main_menu.batch)
-
-e2 = enemy.Enemy("name", "color", 0.1, 1, 45, 46, 1, main_menu.batch)
-e1 = enemy.Enemy("name", "color", 0.1, 1, 53, 54, 1, main_menu.batch)
-e3 = enemy.Enemy("name", "color", 0.1, 1, 44, 46, 1, main_menu.batch)
-
-el = [e2, e1, e3]
-
-b.find_closest_enemy(el)
+(y, x) = map.find_start()
+e = enemy.Enemy("name", "purple", 0.1, 1, x, y, 151, main_menu.batch)
 
 # testare
 # main_menu.hide()
